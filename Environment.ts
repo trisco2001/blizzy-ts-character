5e6fd0ab1fdfd1b40c6341018e1f0b7f41744cca
Tristan Leonard <trisco2001@gmail.com>
2020-01-03 18:36:06 -0800

    Updated dependencies.

--- Environment.ts
+++
@@ -1,9 +0,0 @@
-export class Environment {
-    baseUrl: string
-    locale: string
-    
-    constructor() {
-        this.baseUrl = process.env["BASE_URL"]!
-        this.locale = process.env["LOCALE"]!
-    }
-}
